a7d2469fc9cbf56d3e0e525004603c007221ad94
DipaIi Sarjare <105915562+DipaliSarjare@users.noreply.github.com>
2023-07-07 11:02:35 +0530

    Update EmployeeException.java
    update

--- week1-tasks/EmployeeManagementSystem/src/com/Ems/Exception/EmployeeException.java
+++ week1-tasks/EmployeeManagementSystem/src/com/Ems/Exception/EmployeeException.java
@@ -2,6 +2,10 @@ package com.Ems.Exception;
 
 public class EmployeeException extends Exception{
 
+	public EmployeeException() {
+		super();
+	}
+	
 	public EmployeeException(String message) {
 		super(message);
 		
